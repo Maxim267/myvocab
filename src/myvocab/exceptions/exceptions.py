8b64863c0a2fa30e097e643c74b314fc09ee42e1
Nazarenko Maksim <MNazarenko@bellintegrator.com>
2026-02-19 15:51:11 +0300

    Update comments

--- src/myvocab/exceptions/exceptions.py
+++ src/myvocab/exceptions/exceptions.py
@@ -1,139 +1,139 @@
 class VocabError(Exception):
-    """The base Vocab error."""
+    """ The base Vocab error. """
 
 class DirectoryIsMountError(VocabError):
-    """Raised when a directory is the root of a mounted filesystem."""
+    """ Raised when a directory is the root of a mounted filesystem. """
     def __init__(self, directory, message="Directory is the root of a mounted filesystem:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class DirectoryIsSystemRootError(VocabError):
-    """Raised when a directory is the System Root."""
+    """ Raised when a directory is the System Root. """
     def __init__(self, directory, message="Directory is the System root:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class DirectoryExclamationMarkError(VocabError):
-    """Raised when a directory has the leading exclamation mark."""
+    """ Raised when a directory has the leading exclamation mark. """
     def __init__(self, directory, message="Directory has the leading exclamation mark:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class DirectoryNotExistError(VocabError):
-    """Raised when a directory is not exist."""
+    """ Raised when a directory is not exist. """
     def __init__(self, directory, message="Directory does not exist:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
     
 class DirectoryIsNotFileError(VocabError):
-    """Raised when a directory is not a file."""
+    """ Raised when a directory is not a file. """
     def __init__(self, directory, message="Directory is not a file:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class DirectoryIsNotFolderError(VocabError):
-    """Raised when a directory is not a folder."""
+    """ Raised when a directory is not a folder. """
     def __init__(self, directory, message="The path is not a folder:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class FileNameIsNotFileError(VocabError):
-    """Raised when a filename is invalid."""
+    """ Raised when a filename is invalid. """
     def __init__(self, directory, message="Invalid filename:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class NonBooleanValueError(VocabError):
-    """Raised when a value is not of type boolean."""
+    """ Raised when a value is not of type boolean. """
     def __init__(self, directory, message="The value is not of type boolean:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class IndexOutOfRangeError(VocabError):
-    """Raised when an index out of range."""
+    """ Raised when an index out of range. """
     def __init__(self, directory, message="The index out of range:"):
         self.directory = directory
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.directory}'"
 
 class IdentifierOutOfRangeError(VocabError):
-    """Raised when an identifier out of range."""
+    """ Raised when an identifier out of range. """
     def __init__(self, identifier: int, out_range: range, message: str ="The identifier out of range:"):
         self.identifier = identifier
         self.range = out_range
         self.message = message   
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} {self.identifier} not in {self.range}"
 
 class FileIsNotFoundError(VocabError):
-    """Raised when a file is not found."""
+    """ Raised when a file is not found. """
     def __init__(self, file, message="The file is not found:"):
         self.file = file
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.file}'"
 
 class FileIsEmptyError(VocabError):
-    """Raised when a file is empty."""
+    """ Raised when a file is empty. """
     def __init__(self, file, message="The file is empty:"):
         self.file = file
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.file}'"
 
 class VariableIsNotFoundError(VocabError):
-    """Raised when a variable is not found."""
+    """ Raised when a variable is not found. """
     def __init__(self, variable, message="The variable is not found:"):
         self.variable = variable
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} '{self.variable}'"
 
 class ChunkSizeSmallError(VocabError):
-    """Raised when a chunk size is too small."""
+    """ Raised when a chunk size is too small. """
 
     def __init__(self, size, expected_size, message="The chunk size is too small:"):
         self.size = size
@@ -141,5 +141,38 @@ class ChunkSizeSmallError(VocabError):
         self.message = message
         super().__init__(self.message)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.message} {self.size} (expected {self.expected_size})"
+
+class FetchIAMtokenError(VocabError):
+    """ Raised when failing to fetch the IAM token. """
+
+    def __init__(self, extra_message, message="Failed to fetch IAM token"):
+        self.extra_message = extra_message
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.message} {self.extra_message}"
+
+class FetchSupportedLanguagesError(VocabError):
+    """ Raised when failing to fetch the supported languages. """
+
+    def __init__(self, extra_message, message="Failed to fetch the supported languages"):
+        self.extra_message = extra_message
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.message} {self.extra_message}"
+
+class TargetLanguageCodeIsNotFoundError(VocabError):
+    """ Raised when a target language is not found. """
+
+    def __init__(self, target_language, message="The target language code is not found:"):
+        self.target_language = target_language
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.message} `{self.target_language}`"
